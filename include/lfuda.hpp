96d611e0c71f1762b59e8738b32368f97721e609
ajlekcahdp4 <alex.rom23@mail.ru>
2022-08-01 22:09:27 +0300

    write erase() and insert(), draft promote()

--- include/lfuda.hpp
+++ include/lfuda.hpp
@@ -15,38 +15,47 @@ template <typename T, typename KeyT = int> class lfuda_t
     //=====================================local_node_t=========================================
     class local_node_t
     {
-        using freq_node_it_t = typename std::map<KeyT, local_node_t>::iterator;
+        using local_list_t    = typename std::list<local_node_t>;
+        using local_list_it_t = typename local_list_t::iterator;
+        using freq_node_it_t  = typename std::map<freq_t, local_list_t>::iterator;
 
         T data_;
         KeyT key_;
         freq_t freq_;
-        freq_node_it_t freq_node_;
+        freq_node_it_t freq_list_;
 
       public:
-        local_node_t (KeyT key, freq_t freq, T data) : key_ (key), freq_ (freq), data_ (data) {}
+        local_node_t (T data, freq_t freq, KeyT key) : key_ (key), freq_ (freq), data_ (data) {}
+        local_node_t (T data, freq_t freq, KeyT key, freq_node_it pos)
+            : key_ (key), freq_ (freq), data_ (data), freq_list_ (pos)
+        {
+        }
 
         T data () const { return data_; }
         KeyT key () const { return key_; }
         freq_t freq () const { return freq_; }
+        freq_node_it_t freq_node () { return freq_list_; }
 
         freq_t set_freq (freq_t freq) { return freq_ = freq; }
-        void set_freq_node (freq_node_it_t freq_node) { freq_node_ = freq_node; }
-        freq_node_it_t &freq_node () { return freq_node_; }
+        void set_freq_node (freq_node_it_t freq_node) { freq_list_ = freq_node; }
+        freq_node_it_t &freq_list () { return freq_list_; }
     };
     using local_list_t    = typename std::list<local_node_t>;
     using local_list_it_t = typename local_list_t::iterator;
-    using freq_node_it_t  = typename std::map<KeyT, local_node_t>::iterator;
+    using freq_node_it_t  = typename std::map<freq_t, local_list_t>::iterator;
 
     std::unordered_map<KeyT, local_list_it_t> table_;
     std::map<freq_t, local_list_t> rb_tree_;
 
     size_t capacity_ = 0;
     size_t size_     = 0;
+    size_t age_;
 
   public:
     explicit lfuda_t (size_t cap) : capacity_ (cap) {}
 
     size_t size () const { return size_; }
+    size_t age () const { return age_; }
     size_t capacity () const { return capacity_; }
 
     bool full () const { return size_ == capacity_; }
@@ -58,9 +67,9 @@ template <typename T, typename KeyT = int> class lfuda_t
         {
             if ( full () )
             {
-                erase (key, slow_get_page);
+                erase ();
             }
-            insert ();
+            insert (key, slow_get_page);
 
             return false;
         }
@@ -70,8 +79,59 @@ template <typename T, typename KeyT = int> class lfuda_t
         return true;
     }
 
-    // erase ();
-    // insert();
-    // promote();
+  private:
+    void erase ()
+    {
+        // find to erase
+        auto last_local_list_it       = rb_tree_.begin ();
+        local_list_t &last_local_list = last_local_list_it->second;
+        auto freq                     = last_local_list_it->first;
+        auto &to_erase                = last_local_list.back ();
+
+        // erase
+        table_.erase (to_erase.key ());
+        last_local_list.pop_back ();
+        age_ = freq;
+        size_--;
+
+        // remove if empty
+        if ( last_local_list.empty () && freq != age_ )
+            rb_tree_.erase (to_erase.freq ());   // log(N)
+    }
+
+    template <typename F> void insert (KeyT key, F slow_get_page)
+    {
+        // insert in rb tree or find local_list with freq == 1
+        freq_node_it_t ins_list_it;
+        if ( rb_tree_.empty () || rb_tree_.begin ()->second.begin ()->freq () != age_ )
+            ins_list_it = rb_tree_.emplace (age_, local_list_t);   // log(N)
+        ins_list_it->emplace_front (local_node_t (slow_get_page (key), age_, key, ins_list_it));
+        // insert request into table_ and rb_tree
+        table_[key] = ins_list_it->begin ();
+        size_++;
+    }
+
+    void promote (local_list_it_t &found)
+    {
+        freq_node_it_t parent_list_it = found->freq_list ();
+        freq_node_it_t next_list_it   = std::next (parent_list_it);
+        auto &parent_list             = parent_list_it->second;
+        auto &next_list               = next_list_it->second;
+
+        // find new freq list for promoted local node
+        auto new_freq = age_ + found->freq () + 1;
+        if ( next_list_it == rb_tree_.end () || next_list.front ().freq () != new_freq )
+            rb_tree_.emplace (new_freq);   // wrong
+        local_list_t &next_freq_list = std::next (parent_list_it)->second;   // wrong
+
+        // actually promote
+        next_freq_list.splice (next_freq_list.begin (), parent_list, found);
+
+        // remove if empty
+        if ( parent_list.empty () )
+            rb_tree_.erase (freq);
+    }
 };
 }   // namespace cache
+
+// insert freq_list into local_node
