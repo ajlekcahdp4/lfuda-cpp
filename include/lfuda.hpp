10e07650a0947d0dca57cc99175b39e40ce31d0c
ajlekcahdp4 <alex.rom23@mail.ru>
2022-08-03 19:38:01 +0300

    add license

--- include/lfuda.hpp
+++ include/lfuda.hpp
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <alex.rom23@mail.ru> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Alex Romanov
+ * ----------------------------------------------------------------------------
+ */
+
 #pragma once
 
 #include <algorithm>
